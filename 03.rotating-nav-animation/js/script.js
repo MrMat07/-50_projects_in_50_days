f1708e57c2fbf888b2812b09e6ccab11aebf2b0f
Matias Monti <matigaris@gmail.com>
2023-04-23 20:22:48 -0300

    03.rotating-nav-animation

---
+++ 03.rotating-nav-animation/js/script.js
@@ -0,0 +1,11 @@
+// DOM elements
+const container = document.querySelector('.container');
+
+// Utility functions
+const clickOpenNav=()=>{
+    container.classList.add('show-nav');
+};
+
+const clickCloseNav=()=>{
+    container.classList.remove('show-nav');
+};
